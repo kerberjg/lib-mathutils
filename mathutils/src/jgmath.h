c53097bf46af07bf76806fb1319c425ba1fc7f72
James Game <james.game.64@gmail.com>
2014-10-20 20:44:47 +0200

    Added stub comments

--- mathutils/src/jgmath.h
+++ mathutils/src/jgmath.h
@@ -4,12 +4,17 @@
  *  Created on: Oct 20, 2014
  *      Author: mrgame64
  */
+#import <math.h>
 
 #ifndef JGMATH_H_
 #define JGMATH_H_
 
+//Functions
 
 
+//Vectors
+
+//Matrixes
 
 
 #endif /* JGMATH_H_ */
