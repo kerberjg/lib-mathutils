aaf9386fe7eb8e23847b9ff3d7420c5eeac6ae7e
James Game <james.game.64@gmail.com>
2014-11-06 21:09:31 +0100

    Placeholder compile test file

---
+++ mathutils/src/test.cpp
@@ -0,0 +1,21 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Nov 4, 2014
+ *      Author: mrgame64
+ */
+
+#include "jgmath.h"
+#include <iostream>
+#include "stdio.h"
+
+using namespace std;
+
+int main() {
+	Vec2* v = new Vec2(0, 0);
+
+	cout << v->x << endl;
+
+
+	return 0;
+}
