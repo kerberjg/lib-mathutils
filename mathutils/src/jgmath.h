7c7cc544f546b0b2af08e6011a20050f63c5067e
James Game <james.game.64@gmail.com>
2014-11-10 22:18:48 +0100

    Added Vec2 matrix multiplication operators

--- mathutils/src/jgmath.h
+++ mathutils/src/jgmath.h
@@ -96,7 +96,11 @@ class Vec2 {
 		Vec2&	operator-= (const Vec2& v) { x -= v.x; y -= v.y; return *this; }
 		Vec2&	operator*= (const Vec2& v) { x *= v.x; y *= v.y; return *this; }
 		Vec2&	operator*= (const fp v) { x *= v; y *= v; return *this; }
-		Vec2&	operator*= (const Matrix3& m) { ; return *this; }; //TODO: code Matrix3 operators
+		Vec2&	operator*= (const Matrix3& m) { fp x = this->x * m.val[0] + this->y * m.val[3] + m.val[6];
+												fp y = this->x * m.val[1] + this->y * m.val[4] + m.val[7];
+												this->x = x;
+												this->y = y;
+												return *this; };
 		Vec2&	operator/= (const Vec2& v) { x /= v.x; y /= v.y; return *this; }
 		Vec2&	operator/= (const fp v) { x /= v; y /= v; return *this; }
 		Vec2	operator- () const { return Vec2( -x, -y ); }
@@ -104,7 +108,8 @@ class Vec2 {
 		Vec2	operator- (const Vec2& v) const { return Vec2 (x - v.x, y - v.y); }
 		Vec2	operator* (const Vec2& v) const { return Vec2 (x * v.x, y * v.y); }
 		Vec2	operator* (const fp v) const { return Vec2 (x * v, y * v); }
-		Vec2	operator* (const Matrix3 m) { return Vec2 (); }; //TODO: code Matrix3 operators
+		Vec2	operator* (const Matrix3 m) { return Vec2 (	x * m.val[0] + y * m.val[3] + m.val[6],
+															x * m.val[1] + y * m.val[4] + m.val[7]); };
 		Vec2	operator/ (const Vec2& v) const { return Vec2 (x / v.x, y / v.y); }
 		Vec2	operator/ (const fp v) const { return Vec2 (x / v, y / v); }
 		bool	operator== (const Vec2& v) const { return (x == v.x && y == v.y); }
@@ -133,8 +138,8 @@ class Vec2 {
 
 		void lerp(Vec2& v, fp alpha);
 
-		bool epsilon_equals(Vec2& v, fp epsilon);
-		bool epsilon_equals(fp x, fp y, fp epsilon);
+		inline bool epsilon_equals(Vec2& v, fp epsilon);
+		inline bool epsilon_equals(fp x, fp y, fp epsilon);
 		inline bool is_zero();
 };
 
