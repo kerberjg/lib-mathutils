e178dbf70e1644378d06ed88d469eab3824c85d0
James Game <james.game.64@gmail.com>
2014-11-23 22:30:25 +0100

    Added new functions (to be defined) and data types

--- mathutils/src/mathutils.hpp
+++ mathutils/src/mathutils.hpp
@@ -29,23 +29,27 @@ typedef float fp;
 /*
  * 		Mathematic constants
  */
+
+//PI
 const fp PI = 3.14159265358979323846264338327950288419716939937510;
 const fp HALF_PI = 0.5 * PI;
 const fp TWO_PI = 2.0 * PI;
 const fp TWO_PI_INV = 1.0 / TWO_PI;
-
+//Degrees-radians conversion
 const fp radDeg = 180.0 / PI;
 const fp degRad = PI / 180.0;
 const fp radFull = 2 * PI;
 const fp degFull = 360.0;
+//Others
+const fp E = 2.71828182845904523536028747135266249775724709369995; //Euler's number
 
 
 /*
  * 		Mathematic functions
  * 		maths.cpp
  */
+//Sine-cosine
 #ifndef PRECISE
-//Sine default precision bits
 #ifndef SIN_BITS
 #define SIN_BITS 16
 #endif
@@ -65,7 +69,7 @@ fp pre_cos(fp rad);
 fp sin_deg(fp deg);
 fp cos_deg(fp deg);
 
-//Atan
+//Atan2
 #ifndef PRECISE
 #define ATAN2_BITS 8 // Adjust for accuracy.
 #define ATAN2_BITS2 ATAN2_BITS << 1
@@ -106,3 +110,43 @@ extern u64 m64;
 extern u32 m32;
 u64 murmurhash3_64();
 u32 murmurhash3_32();
+
+
+/*
+ * 		Limiting&interpolating numbers
+ * 		rngirp.cpp
+ */
+
+//Clamping
+int clamp(int x, int max, int min);
+fp clamp(fp x, fp max, fp min);
+//Checking ranges
+int is_range(int x, int max, int min);
+fp is_range(fp x, fp max, fp min);
+//Interpolating
+fp lerp(fp x, fp start, fp end);
+
+
+/*
+ * 		Rounding&comparing numbers
+ * 		round.cpp
+ */
+#define FLOAT_ROUNDING_ERROR 1e-6
+
+//Rounding
+int round(fp x);
+int floor(fp x);
+int ceil(fp x);
+//Comparing
+bool is_zero(fp x);
+bool is_zero(fp x, fp error);
+bool is_equal(fp x);
+bool is_equal(fp x, fp error);
+
+/*
+ * 		Very precise numbers
+ * 		xprec.hpp
+ */
+class BigFloat;
+class u128;
+class u256;
