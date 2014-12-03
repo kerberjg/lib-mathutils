e3e508d8f20783223abf852e9aa7ce1150111c62
James Game <james.game.64@gmail.com>
2014-12-03 21:55:07 +0100

    Put xprec and vecmat inside the math namespace

--- mathutils/src/vecmat.hpp
+++ mathutils/src/vecmat.hpp
@@ -9,98 +9,99 @@
 
 #include "mathutils.hpp"
 
-using namespace math;
+namespace math {
 
-//Matrices
-class Matrix3 {
-	public:
-		static const int M00 = 0;
-		static const int M01 = 3;
-		static const int M02 = 6;
-		static const int M10 = 1;
-		static const int M11 = 4;
-		static const int M12 = 7;
-		static const int M20 = 2;
-		static const int M21 = 5;
-		static const int M22 = 8;
+	//Matrices
+	class Matrix3 {
+		public:
+			static const int M00 = 0;
+			static const int M01 = 3;
+			static const int M02 = 6;
+			static const int M10 = 1;
+			static const int M11 = 4;
+			static const int M12 = 7;
+			static const int M20 = 2;
+			static const int M21 = 5;
+			static const int M22 = 8;
 
-		fp val[9];
-		fp tmp[9];
+			fp val[9];
+			fp tmp[9];
 
-		char* to_string();
-};
+			char* to_string();
+	};
 
-class Matrix4 {};
+	class Matrix4 {};
 
-//Vectors
-class Vec2 {
-	public:
-		fp x, y;
+	//Vectors
+	class Vec2 {
+		public:
+			fp x, y;
 
-		Vec2();
-		Vec2(const Vec2& v);
-		Vec2(fp x, fp y);
+			Vec2();
+			Vec2(const Vec2& v);
+			Vec2(fp x, fp y);
 
-		Vec2* copy();
+			Vec2* copy();
 
-		//Operators
-		Vec2&	operator= (const Vec2& v) { x = v.x; y = v.y; return *this; }
-		Vec2&	operator+= (const Vec2& v) { x += v.x; y += v.y; return *this; }
-		Vec2&	operator-= (const Vec2& v) { x -= v.x; y -= v.y; return *this; }
-		Vec2&	operator*= (const Vec2& v) { x *= v.x; y *= v.y; return *this; }
-		Vec2&	operator*= (const fp v) { x *= v; y *= v; return *this; }
-		/*Vec2&	operator*= (const Matrix3& m) { fp x = this->x * m.val[0] + this->y * m.val[3] + m.val[6];
-												fp y = this->x * m.val[1] + this->y * m.val[4] + m.val[7];
-												this->x = x;
-												this->y = y;
-												return *this; };*/
-		Vec2&	operator/= (const Vec2& v) { x /= v.x; y /= v.y; return *this; }
-		Vec2&	operator/= (const fp v) { x /= v; y /= v; return *this; }
-		Vec2	operator- () const { return Vec2( -x, -y ); }
-		Vec2	operator+ (const Vec2& v) const { return Vec2 (x + v.x, y + v.y); }
-		Vec2	operator- (const Vec2& v) const { return Vec2 (x - v.x, y - v.y); }
-		Vec2	operator* (const Vec2& v) const { return Vec2 (x * v.x, y * v.y); }
-		Vec2	operator* (const fp v) const { return Vec2 (x * v, y * v); }
-		/*Vec2	operator* (const Matrix3 m) { return Vec2 (	x * m.val[0] + y * m.val[3] + m.val[6],
-															x * m.val[1] + y * m.val[4] + m.val[7]); };*/
-		Vec2	operator/ (const Vec2& v) const { return Vec2 (x / v.x, y / v.y); }
-		Vec2	operator/ (const fp v) const { return Vec2 (x / v, y / v); }
-		bool	operator== (const Vec2& v) const { return (x == v.x && y == v.y); }
-		bool	operator!= (const Vec2& v) const { return !(x == v.x && y == v.y); }
+			//Operators
+			Vec2&	operator= (const Vec2& v) { x = v.x; y = v.y; return *this; }
+			Vec2&	operator+= (const Vec2& v) { x += v.x; y += v.y; return *this; }
+			Vec2&	operator-= (const Vec2& v) { x -= v.x; y -= v.y; return *this; }
+			Vec2&	operator*= (const Vec2& v) { x *= v.x; y *= v.y; return *this; }
+			Vec2&	operator*= (const fp v) { x *= v; y *= v; return *this; }
+			/*Vec2&	operator*= (const Matrix3& m) { fp x = this->x * m.val[0] + this->y * m.val[3] + m.val[6];
+													fp y = this->x * m.val[1] + this->y * m.val[4] + m.val[7];
+													this->x = x;
+													this->y = y;
+													return *this; };*/
+			Vec2&	operator/= (const Vec2& v) { x /= v.x; y /= v.y; return *this; }
+			Vec2&	operator/= (const fp v) { x /= v; y /= v; return *this; }
+			Vec2	operator- () const { return Vec2( -x, -y ); }
+			Vec2	operator+ (const Vec2& v) const { return Vec2 (x + v.x, y + v.y); }
+			Vec2	operator- (const Vec2& v) const { return Vec2 (x - v.x, y - v.y); }
+			Vec2	operator* (const Vec2& v) const { return Vec2 (x * v.x, y * v.y); }
+			Vec2	operator* (const fp v) const { return Vec2 (x * v, y * v); }
+			/*Vec2	operator* (const Matrix3 m) { return Vec2 (	x * m.val[0] + y * m.val[3] + m.val[6],
+																x * m.val[1] + y * m.val[4] + m.val[7]); };*/
+			Vec2	operator/ (const Vec2& v) const { return Vec2 (x / v.x, y / v.y); }
+			Vec2	operator/ (const fp v) const { return Vec2 (x / v, y / v); }
+			bool	operator== (const Vec2& v) const { return (x == v.x && y == v.y); }
+			bool	operator!= (const Vec2& v) const { return !(x == v.x && y == v.y); }
 
-		//TODO: add static functions
-		void norm();
-		void limit(fp l);
-		void clamp(fp min, fp max);
-		void set_angle(fp rad);
-		void set_len(fp l);
-		void rotate(fp rad);
-		void rotate_90(int i);
-		inline void set_zero();
+			//TODO: add static functions
+			void norm();
+			void limit(fp l);
+			void clamp(fp min, fp max);
+			void set_angle(fp rad);
+			void set_len(fp l);
+			void rotate(fp rad);
+			void rotate_90(int i);
+			inline void set_zero();
 
-		inline fp len();
-		inline fp len2();
-		inline fp angle();
-		inline fp dotp(Vec2& v);
-		inline fp dotp(fp x, fp y);
-		inline fp cross(Vec2& v);
-		inline fp cross(fp x, fp y);
-		inline fp dist(Vec2& v);
-		inline fp dist(fp x, fp y);
-		inline fp dist2(Vec2& v);
-		inline fp dist2(fp x, fp y);
+			inline fp len();
+			inline fp len2();
+			inline fp angle();
+			inline fp dotp(Vec2& v);
+			inline fp dotp(fp x, fp y);
+			inline fp cross(Vec2& v);
+			inline fp cross(fp x, fp y);
+			inline fp dist(Vec2& v);
+			inline fp dist(fp x, fp y);
+			inline fp dist2(Vec2& v);
+			inline fp dist2(fp x, fp y);
 
-		void lerp(Vec2& v, fp alpha);
+			void lerp(Vec2& v, fp alpha);
 
-		inline bool epsilon_equals(Vec2& v, fp epsilon);
-		inline bool epsilon_equals(fp x, fp y, fp epsilon);
-		inline bool is_zero();
+			inline bool epsilon_equals(Vec2& v, fp epsilon);
+			inline bool epsilon_equals(fp x, fp y, fp epsilon);
+			inline bool is_zero();
 
-		char* to_string();
-};
+			char* to_string();
+	};
 
-class Vec3 {};
+	class Vec3 {};
 
-class Vec4 {};
+	class Vec4 {};
+}
 
 #endif /* JGMATH_H_ */
--- mathutils/src/xprec.hpp
+++ mathutils/src/xprec.hpp
@@ -13,61 +13,61 @@
 
 #include "mathutils.hpp"
 
-using namespace math;
+namespace math {
 
-//96bit floating point
-class float128 {
-	public:
-		u64 v;
-		s64 s;
+	//96bit floating point
+	class float128 {
+		public:
+			u64 v;
+			s64 s;
 
-		//Constructors
-		float128();
-		float128(u64 value, s64 scale);
+			//Constructors
+			float128();
+			float128(u64 value, s64 scale);
 
-		//Operators
-		float128&	operator= (const float128& r) { v = r.v; s = r.s; return *this; }
-		float128&	operator+= (const float128& r) { v += r.v; s = math::max(s,r.s); return *this; }
-		float128&	operator-= (const float128& r) { v -= r.v; s = math::max(s,r.s); return *this; }
-		float128&	operator*= (const float128& r) { v *= r.v; s += r.s; return *this; }
-		float128&	operator/= (const float128& r) { v /= r.v; s -= r.s; return *this; }
-		//TODO: division of float128 to be investigated
-		//source: https://docs.oracle.com/javase/1.5.0/docs/api/java/math/BigDecimal.html#divide(BigDecimal divisor)
+			//Operators
+			float128&	operator= (const float128& r) { v = r.v; s = r.s; return *this; }
+			float128&	operator+= (const float128& r) { v += r.v; s = math::max(s,r.s); return *this; }
+			float128&	operator-= (const float128& r) { v -= r.v; s = math::max(s,r.s); return *this; }
+			float128&	operator*= (const float128& r) { v *= r.v; s += r.s; return *this; }
+			float128&	operator/= (const float128& r) { v /= r.v; s -= r.s; return *this; }
+			//TODO: division of float128 to be investigated
+			//source: https://docs.oracle.com/javase/1.5.0/docs/api/java/math/BigDecimal.html#divide(BigDecimal divisor)
 
-		float128		operator+ (const float128& r) const { return float128 (v + r.v, math::max(s,r.s)); };
-		float128		operator- (const float128& r) const { return float128 (v - r.v, math::max(s,r.s)); };
-		float128		operator* (const float128& r) const { return float128 (v * r.v, s + r.s); };
-		float128		operator/ (const float128& r) const { return float128 (v / r.v, s - r.s); };
+			float128		operator+ (const float128& r) const { return float128 (v + r.v, math::max(s,r.s)); };
+			float128		operator- (const float128& r) const { return float128 (v - r.v, math::max(s,r.s)); };
+			float128		operator* (const float128& r) const { return float128 (v * r.v, s + r.s); };
+			float128		operator/ (const float128& r) const { return float128 (v / r.v, s - r.s); };
 
-		bool	operator== (const float128& r) {
-			if(v != r.v)
-				return false;
-			else
-				if(v == 0)
-					return true;
-				else
-					return (s == r.s);
-		};
-		bool	operator!= (const float128& r) {
-			if(v != r.v)
-				return true;
-			else
-				if(v == 0)
+			bool	operator== (const float128& r) {
+				if(v != r.v)
 					return false;
 				else
-					return (s != r.s);
-		};
+					if(v == 0)
+						return true;
+					else
+						return (s == r.s);
+			};
+			bool	operator!= (const float128& r) {
+				if(v != r.v)
+					return true;
+				else
+					if(v == 0)
+						return false;
+					else
+						return (s != r.s);
+			};
 
-		//Conversion
-		float128& double_to_float128(double d);
-		double to_double();
-		char* to_string();
-};
-//Big integers
-class u128;
-class u256;
+			//Conversion
+			float128& double_to_float128(double d);
+			double to_double();
+			char* to_string();
+	};
+	//Big integers
+	class u128;
+	class u256;
 
-
+}
 
 
 #endif /* XPREC_HPP_ */
