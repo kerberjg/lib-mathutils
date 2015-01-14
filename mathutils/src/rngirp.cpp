d661e9e67a7b29baa18bd6e37888ed6f8fa97b40
mrgame64 <mrgame64@jamess-mbp.lan>
2015-01-14 21:12:38 +0100

    Implemented "Limiting&interpolating numbers"

---
+++ mathutils/src/rngirp.cpp
@@ -0,0 +1,36 @@
+/*
+ * rngirp.cpp
+ *
+ *  Created on: Jan 14, 2015
+ *      Author: mrgame64
+ */
+
+#include "mathutils.hpp"
+
+using namespace math;
+
+int clamp(int x, int max, int min) {
+	return ( x > max ? max : (x < min ? min : x));
+}
+
+fp clamp(fp x, fp max, fp min) {
+	return ( x > max ? max : (x < min ? min : x));
+}
+
+bool is_range(int x, int max, int min, bool extremes) {
+	if(extremes)
+		return ( min <= x && x <= max );
+	else
+		return ( min < x && x < max);
+}
+
+bool is_range(fp x, fp max, fp min, bool extremes) {
+	if(extremes)
+		return ( min <= x && x <= max );
+	else
+		return ( min < x && x < max);
+}
+
+fp lerp(fp x, fp start, fp end) {
+	return x * (end - start) + start;
+}
