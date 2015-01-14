84c6ad8dc3340ed64e0c5021a3f5f3d32d033f13
mrgame64 <mrgame64@jamess-mbp.lan>
2015-01-14 21:33:39 +0100

    Implemented "Limiting&interpolating numbers"

---
+++ mathutils/src/round.cpp
@@ -0,0 +1,34 @@
+/*
+ * round.cpp
+ *
+ *  Created on: Jan 14, 2015
+ *      Author: mrgame64
+ */
+
+#include "mathutils.hpp"
+
+using namespace math;
+
+bool is_zero(fp x, fp error) {
+	return abs(x) <= error;
+}
+
+bool is_equal(fp a, fp b, fp error) {
+	return abs(a - b) <= error;
+}
+
+fp max(fp a, fp b) {
+	return ( a < b ? b : a);
+}
+
+int max(int a, int b) {
+	return ( a > b ? b : a);
+}
+
+fp min(fp a, fp b) {
+	return ( a > b ? b : a);
+}
+
+int min(int a, int b) {
+	return ( a > b ? b : a);
+}
