374a9b3b0ca208b6d509baf0b494d509ba6ae836
mrgame64 <james.game.64@gmail.com>
2015-01-19 18:11:21 +0100

    Implemented Vec3::norm(), Vec3::len() and Vec3::len2()

--- mathutils/src/Vec3.cpp
+++ mathutils/src/Vec3.cpp
@@ -17,3 +17,22 @@ Vec3::Vec3(const Vec3& v): x(v.x), y(v.y), z(v.z){
 
 Vec3::Vec3(fp x, fp y, fp z): x(x), y(y), z(z) {
 }
+
+void Vec3::norm() {
+	if(x == 0 && y == 0 && z == 0)
+		return;
+
+	fp l = len();
+	x /= l;
+	y /= l;
+	z /= l;
+}
+
+
+fp Vec3::len() {
+	return sqrt(x*x + y*y + z*z);
+}
+
+fp Vec3::len2() {
+	return x*x + y*y + z*z;
+}
