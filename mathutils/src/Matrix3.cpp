6a5886bb982e4f2f623b9eaf65637893de2ee8b9
mrgame64 <james.game.64@gmail.com>
2015-01-23 11:58:36 +0100

    Corrected determinant calculation and implemented setters/getters

--- mathutils/src/Matrix3.cpp
+++ mathutils/src/Matrix3.cpp
@@ -46,9 +46,96 @@ void Matrix3::trnsp() {
 }
 
 inline fp Matrix3::det() {
-	return  +v[1][0] * (v[1][1]*v[2][2] - v[2][1]*v[1][2])
-			-v[1][0] * (v[0][1]*v[2][2] - v[2][1]*v[0][2])
-			+v[1][0] * (v[0][1]*v[1][2] - v[1][1]*v[0][2]);
+	return  +v[0][0] * (v[1][1]*v[2][2] - v[1][2]*v[2][1])
+			-v[0][1] * (v[1][0]*v[2][2] - v[1][2]*v[2][0])
+			+v[0][2] * (v[1][0]*v[2][0] - v[1][1]*v[2][0]);
 }
 
+void Matrix3::set_rot(fp rad) {
+	fp cos = math::cos(rad);
+	fp sin = math::sin(rad);
 
+	v[0][0] = cos;
+	v[1][0] = sin;
+	v[2][0] = 0;
+
+	v[0][1] = -sin;
+	v[1][1] = cos;
+	v[2][1] = 0;
+
+	v[0][2] = 0;
+	v[1][2] = 0;
+	v[2][2] = 1;
+}
+
+inline void Matrix3::set_rotd(fp deg) {
+	set_rot(deg * degRad);
+}
+
+void Matrix3::set_rot(Vec3& axis, fp rad) {
+	fp cos = math::cos(rad);
+	fp sin = math::sin(rad);
+	fp oc = 1.0 - cos;
+
+	v[0][0] = oc * axis.x * axis.x + cos;
+	v[1][0] = oc * axis.x * axis.y - axis.z * sin;
+	v[2][0] = oc * axis.z * axis.x + axis.y * sin;
+	v[0][1] = oc * axis.x * axis.y + axis.z * sin;
+	v[1][1] = oc * axis.y * axis.y + cos;
+	v[2][1] = oc * axis.y * axis.z - axis.x * sin;
+	v[0][2] = oc * axis.z * axis.x - axis.y * sin;
+	v[1][2] = oc * axis.y * axis.z + axis.x * sin;
+	v[2][2] = oc * axis.z * axis.z + cos;
+}
+
+inline void Matrix3::set_rotd(Vec3& axis, fp deg) {
+	set_rot(axis, deg * degRad);
+}
+
+fp Matrix3::get_rot() {
+	return atan2(v[1][0], v[0][0]);
+}
+
+inline fp Matrix3::get_rotd() {
+	return get_rot() * radDeg;
+}
+
+void Matrix3::set_trn(fp x, fp y) {
+	v[0][0] = 1;
+	v[1][0] = 0;
+	v[2][0] = 0;
+
+	v[0][1] = 0;
+	v[1][1] = 1;
+	v[2][1] = 0;
+
+	v[0][2] = x;
+	v[1][2] = y;
+	v[2][2] = 1;
+}
+
+inline void Matrix3::set_trn(Vec2& v) {
+	set_trn(v.x, v.y);
+}
+
+void Matrix3::set_scl(fp sx, fp sy) {
+	v[0][0] = sx;
+	v[1][0] = 0;
+	v[2][0] = 0;
+
+	v[0][1] = 0;
+	v[1][1] = sy;
+	v[2][1] = 0;
+
+	v[0][2] = 0;
+	v[1][2] = 0;
+	v[2][2] = 1;
+}
+
+inline void Matrix3::set_scl(Vec2& v) {
+	set_scl(v.x, v.y);
+}
+
+Vec2 Matrix3::get_scl() {
+	return Vec2(v[0][0], v[1][1]);
+}
