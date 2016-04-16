9219d48ea19f160d22d6903e3da140084ba0da7d
Jakub Kerber <kerber.jg@gmail.com>
2016-04-16 12:30:10 +0200

    Removed unused content

--- src/common.h
+++ src/common.h
@@ -10,10 +10,6 @@
 
 #include <stdint.h>
 
-#define InitPointer(pointer) ((pointer) = NULL)
-#define SafeDelete(pointer) if(pointer != NULL) {delete pointer; pointer = NULL;}
-#define SafeDeleteArray(pointer) if(pointer != NULL) {delete [] pointer; pointer = NULL;}
-
 //Data types
 typedef unsigned char u8;
 typedef int8_t s8;
@@ -23,11 +19,5 @@ typedef unsigned int u32;
 typedef int32_t s32;
 typedef unsigned long long int u64;
 typedef int64_t s64;
-
-//Flags
-#define FLAG_Z 0x80
-#define FLAG_N 0x40
-#define FLAG_H 0x20
-#define FLAG_C 0x10
 
 #endif /* COMMON_H_ */
