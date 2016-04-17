5b99311b985dabc16e23bd593800929f3119dd94
Jakub Kerber <kerber.jg@gmail.com>
2016-04-17 12:51:48 +0200

    Updated hashing functions to use strings; removed useless MD algos

--- src/hash.hpp
+++ src/hash.hpp
@@ -7,24 +7,22 @@
  *      Author: Jakub Kerber <kerber.jg@gmail.com>
  */
 
+#include <string>
 #include "mathutils.hpp"
-#include "xprec.hpp"
 
 namespace math {
 
 	//CRC
+	u8 crc8();
 	u16 crc16();
 	u32 crc32();
 	u64 crc64();
 
 	//Message-Digest
-	u128 md2();
-	u128 md3();
-	u128 md4();
-	u128 md5();
+	std::string md5();
 
 	//SHA
-	u256 sha256();
+	std::string sha256();
 
 	//MurmurHash 3
 	u32 murmurhash3_32();
